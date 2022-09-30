5ec391b1787077a2ef5071b9049c69edd5744b62
Jacob Henderson <jacobth495@gmail.com>
2022-09-29 19:47:40 -0500

    Created winner? method for GameBoard module

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,9 @@
 module GameBoard
+  attr_accessor :player
   @@top_array =  [nil, nil, nil]
   @@middle_array = [nil, nil, nil]
   @@bottom_array = [nil, nil, nil]
+
   def show_board
     print @@top_array
     puts ''
@@ -12,12 +14,14 @@ module GameBoard
     print @@bottom_array
     puts ''
   end
+
   def play_round(input)
     puts "t1 t2 t3"
     puts "m1 m2 m3"
     puts "b1 b2 b3"
+    puts "#{@player}'s Turn"
     puts "Choose Where To Play"
-    location = gets.chomp
+    location = gets.chomp.downcase
     case location
     when "t1"
       @@top_array[0] = input
@@ -40,6 +44,64 @@ module GameBoard
     else
       puts "Please choose t1, t2, t3, m1, m2, m3, b1, b2, b3"
     end
+    show_board()
+  end
+
+  def winner?
+    #horizontal 3 in a rows
+    if @@top_array[0] == 'x' && @@top_array[1] == 'x' && @@top_array[2] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@top_array[0] == 'o' && @@top_array[1] == 'o' && @@top_array[2] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@middle_array[0] == 'o' && @@middle_array[1] == 'o' && @@middle_array[2] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@middle_array[0] == 'x' && @@middle_array[1] == 'x' && @@middle_array[2] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[0] == 'x' && @@bottom_array[1] == 'x' && @@bottom_array[2] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[0] == 'o' && @@bottom_array[1] == 'o' && @@bottom_array[2] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    #vertical 3 in a rows
+    elsif @@bottom_array[0] == 'o' && @@middle_array[0] == 'o' && @@top_array[0] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[0] == 'x' && @@middle_array[0] == 'x' && @@top_array[0] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[1] == 'o' && @@middle_array[1] == 'o' && @@top_array[1] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[1] == 'x' && @@middle_array[1] == 'x' && @@top_array[1] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[2] == 'o' && @@middle_array[2] == 'o' && @@top_array[2] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[2] == 'x' && @@middle_array[2] == 'x' && @@top_array[2] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    #diagnal 3 in a rows
+    elsif @@bottom_array[0] == 'x' && @@middle_array[1] == 'x' && @@top_array[2] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[0] == 'o' && @@middle_array[1] == 'o' && @@top_array[2] == 'o'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[2] == 'x' && @@middle_array[1] == 'x' && @@top_array[0] == 'x'
+      puts "Congrats #{@player} is the winner!!!"
+      true
+    elsif @@bottom_array[2] == 'o' && @@middle_array[1] == 'o' && @@top_array[0] == 'o'
+      puts "Congrats #{@player} is the winner!!!" 
+      true
+    else
+      false
+    end
   end
 end
 
@@ -54,9 +116,11 @@ end
 
 playerone = Player.new
 playertwo = Player.new
-
+players = playerone || playertwo
 playerone.show_board
 
-playerone.play_round("x")
-
-playerone.show_board
+until players.winner? == true
+  playerone.play_round("x")
+  break if players.winner? == true
+  playertwo.play_round("o")
+end
